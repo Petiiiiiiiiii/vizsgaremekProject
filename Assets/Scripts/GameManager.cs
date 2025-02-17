3b9fdc9be8485914873153994f44d0ad1d822985
Orosz Péter <70895314+Petiiiiiiiiii@users.noreply.github.com>
2025-02-17 14:48:59 +0100

    asd

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public string player;
+    void Start()
+    {
+        player = "player - G36C";
+    }
+}
