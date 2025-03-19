f3a315c681b0a2e5bc3bc7ae362b673fe0a312b9
Orosz Péter <C20OroszP@szgyatech.local>
2025-03-19 12:16:13 +0100

    ez meg az

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public string player;
+    private GameObject[] allObjects;
+    [SerializeField] private bool isBossDead;
+    public GameObject bossRoom;
+
     void Start()
     {
         player = "player - G36C";
+        allObjects = FindObjectsOfType<GameObject>();
+        isBossDead = false;
+    }
+    private void Update()
+    {
+        UpdateEnemyCount();
+
+        if (isBossDead)
+        {
+            Debug.Log("boss meghalt");
+            bossRoom.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+
+    public void UpdateEnemyCount()
+    {
+        allObjects = FindObjectsOfType<GameObject>();
+
+        int boss = 0;
+
+        foreach (GameObject obj in allObjects)
+        {
+            if (obj.tag == "enemyBOSS")
+            {
+                boss++;
+            }
+        }
+
+        if (boss == 0) isBossDead = true;
+        else isBossDead = false;
+
     }
 }
--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.ShaderGraph;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +9,8 @@ public class HealthSystem : MonoBehaviour
 {
     [SerializeField] private float currentHealth = 100;
     //[SerializeField] private float maxHealth = 100;
+
+    public GameObject damageIndicator;
 
     public Slider healthSlider;
     public TextMeshProUGUI healthUI;
@@ -22,14 +25,32 @@ public class HealthSystem : MonoBehaviour
         {
             healthSlider.value = currentHealth / 100;
             healthUI.text = $"{currentHealth}";
-            if (currentHealth > 75) healthUI.color = Color.white;
-            else if (currentHealth <= 75 && currentHealth > 25) healthUI.color = Color.yellow;
-            else if (currentHealth <= 25) healthUI.color = Color.red;
+            if (currentHealth > 75)
+            {
+                healthUI.color = Color.white;
+            } 
+            else if (currentHealth <= 75 && currentHealth > 25)
+            {
+                healthUI.color = Color.yellow;
+            } 
+            else if (currentHealth <= 25)
+            {
+                healthUI.color = Color.red;
+            } 
+
+            
         }
+    }
+    IEnumerator dmgIndicator() 
+    {
+        damageIndicator.GetComponent<Image>().color = new Color(1, 0, 0, 0.2f);
+        yield return new WaitForSeconds(0.1f);
+        damageIndicator.GetComponent<Image>().color = new Color(1, 0, 0, 0f);
     }
 
     public void TakeDmg(float dmg) 
     {
+        StartCoroutine(dmgIndicator());
         currentHealth -= dmg;
 
         if (currentHealth <= 0)
